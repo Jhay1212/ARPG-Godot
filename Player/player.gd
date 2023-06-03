15e0e20c86ae286af541a486eb2a8e1bd8a49ed5
Jhay1212 <jhayjane78@gmail.com>
2023-06-03 12:08:19 +0800

    Finished Green Ninja Animation
    I created the animation for green ninja for all direction through animation player only.

--- Player/player.gd
+++ Player/player.gd
@@ -1,6 +1,10 @@
 extends CharacterBody2D
 @export var speed: int = 70
+@onready var animations = $AnimationPlayer
 
+func update():
+	var direction = "Down"
+	
 func inputMovement():
 	var inputDirection = Input.get_vector('ui_left', 'ui_right', 'ui_up', 'ui_down')
 	velocity = inputDirection * speed
