dd59b2c01ff90a9668aa22694992c86474d4f78b
Jhay1212 <jhayjane78@gmail.com>
2023-06-03 12:22:31 +0800

    Completed Direction animation on player
    Completed the animation on player with animationplayter

--- Player/player.gd
+++ Player/player.gd
@@ -2,8 +2,17 @@ extends CharacterBody2D
 @export var speed: int = 70
 @onready var animations = $AnimationPlayer
 
-func update():
-	var direction = "Down"
+func updateAnimation():
+	if velocity.length() == 0:
+		animations.stop()
+	else:
+		var direction = "Down"
+		if velocity.x < 0:direction = 'Left'
+		elif velocity.x > 0: direction = 'Right'
+		elif velocity.y < 0: direction = "Up"
+		elif velocity.y > 0: direction = 'Down'
+	
+		animations.play('walk' + direction)
 	
 func inputMovement():
 	var inputDirection = Input.get_vector('ui_left', 'ui_right', 'ui_up', 'ui_down')
@@ -12,3 +21,4 @@ func inputMovement():
 func _physics_process(delta):
 	inputMovement()
 	move_and_slide()
+	updateAnimation()
