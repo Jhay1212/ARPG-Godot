e237be12a08180c28cd0a542cda2c080fcfa2d1a
Jhay1212 <jhayjane78@gmail.com>
2023-06-03 11:36:47 +0800

    Created walk down animation
    I finished creating walk down animation on animation player on green ninja

--- Player/player.gd
+++ Player/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
-@export var speed: int = 40
+@export var speed: int = 70
 
 func inputMovement():
-	var inputDirection = Input.get_vector('ui_left', 'ui_right', 'ui_up', 'up_down')
+	var inputDirection = Input.get_vector('ui_left', 'ui_right', 'ui_up', 'ui_down')
 	velocity = inputDirection * speed
 
 func _physics_process(delta):
